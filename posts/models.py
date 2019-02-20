93312cf7b193a91c235e5cd4f9559089bf777159
tlaz4 <tlazaren@ualberta.ca>
2019-02-19 20:01:19 -0700

    created initial model for a post

--- posts/models.py
+++ posts/models.py
@@ -10,3 +10,37 @@ class User(AbstractUser):
     displayName = models.CharField(max_length=200)
     github = models.URLField(blank=True)
     bio = models.CharField(max_length=256, blank=True)
+
+class Post(models.Model):
+	PLAIN = "text/plain"
+	MARKDOWN = "text/markdown"
+	BASE64 = "application/base64"
+	PNG = "image/png;base64"
+	JPEG = "image/jpeg;base64"
+
+	CONTENTCHOICES = (
+		(PLAIN, "Plain"),
+		(MARKDOWN, "Markdown"),
+		(BASE64, "Base64"),
+		(PNG, "PNG"),
+		(JPEG, "JPEG")
+	)
+
+	VISIBILITY = (
+		("PUBLIC", "Public"),
+		("PRIVATE", "Private")
+	)
+
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	title = models.CharField(max_length=200)
+	source = models.URLField(blank=True)
+	origin = models.URLField(blank=True)
+	description = models.CharField(max_length=400)
+	contentType = models.CharField(max_length=18, choices=CONTENTCHOICES, default=PLAIN)
+	content = models.BinaryField()
+	published = models.DateTimeField()
+	visibility = models.CharField(max_length=7, choices=VISIBILITY, default="PUBLIC")
+	unlisted = models.BooleanField()
+
+	# missing author, comments, and categories
+
